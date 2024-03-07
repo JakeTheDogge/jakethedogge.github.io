cbffa0e9c3824cefbb3645aea308d0f367e58ae2
Senya Dog <senya.dogg@gmail.com>
2024-03-07 13:04:58 +0200

    style changes

--- src/Tetris.jsx
+++ src/Tetris.jsx
@@ -1,7 +1,4 @@
 import './Tetris.scss';
-import fabricLogo from './assets/Fabric_logo-01.png';
-import openBracket from './assets/openBracket.png';
-import closeBracket from './assets/closeBracket.png';
 import {useBoard} from "./utils/useBoard";
 import {useRef} from "react";
 import {ArrowKeyboard} from "./components/ArrowKeyboard";
@@ -21,26 +18,17 @@ export const Tetris = () => {
   const [display, score, level, isGameOver, onKeyDown, onClick, restart] = useBoard();
   const board = useRef();
   board.current?.focus();
-  if (isGameOver) return (
-    <GameOver restart={restart} />
-  )
-
 
   return (
     <div className='page'>
-      <div className='header'>
-        <img src={openBracket} className='bracket'/>
-        Fits any space
-        <img src={closeBracket} className='bracket'/>
-      </div>
       <div className='body'>
         <div className='right-side'>
-          <div className='website'>getfabric.com</div>
         </div>
-        <div className='board' onKeyDown={onKeyDown} ref={board} tabIndex={0}>
-          {display.map((row, index) =>
-            <Row row={row} key={index}/>)}
-        </div>
+        {isGameOver ? <GameOver restart={restart} /> :
+          <div className='board' onKeyDown={onKeyDown} ref={board} tabIndex={0}>
+            {display.map((row, index) =>
+              <Row row={row} key={index}/>)}
+          </div>}
         <div className='left-side'>
           <div className='score'>
             <div className='points'>Points:</div>
@@ -49,7 +37,6 @@ export const Tetris = () => {
             <div className='level-value'>{level}</div>
           </div>
           <ArrowKeyboard onClick={onClick}/>
-          <img src={fabricLogo} className='fabric-logo' alt='Fabric'/>
         </div>
       </div>
     </div>
